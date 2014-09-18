4792671d5d02353789e40ad7e5dc78fe47f8782e
Bening <beningranum@yahoo.com>
2014-09-19 05:55:26 +0700

    saving user profile is now 99% working. all that left was uploading profile picture

--- simaya/controller/api/2.0/profile.js
+++ simaya/controller/api/2.0/profile.js
@@ -3,6 +3,7 @@ module.exports = function(app){
   var user = require("../../../../sinergis/models/user.js")(app)
   var moment = require("moment")
   var ObjectID = app.ObjectID;
+  var fs = require('fs');
   var notification = require('../../../models/notification.js')(app)
   var profileWeb = require('../../profile.js')(app)
       , contacts = require('../../../models/contacts.js')(app)
@@ -124,31 +125,238 @@ module.exports = function(app){
    * @apiSuccess {String} data.notes Personal notes about this user
    */
 
+  var associateEmails = function(count, username, req, res, callback) {
+    if (typeof(req.body["profile.emails"]) == "string") {
+      req.body["profile.emails"] = [req.body["profile.emails"]];
+    }
+    if (count < req.body["profile.emails"].length) {
+      user.associateEmail(username, req.body["profile.emails"][count], function(token, v) {
+        user.activateEmailAssociation(token, req.body["profile.emails"][count], function(r) {
+          associateEmails(count + 1, username, req, res, callback);
+        })
+      })
+    } else {
+      callback();
+    }
+  }
+
   var save = function(req, res) {
     var data = {};
     data.meta = {};
     if (req.user.id) {
+      console.log(1)
       if (req.body) {
         var doc = {};
-        doc.profile = req.body.profile;
-        doc.emailList = req.body.emailList;
-        //console.log("REQBODY = " + JSON.stringify(req.body));
-        var collection = settings.db.collection("user");
-
-        collection.findAndModify({username: req.user.id}, {_id:1}, {$set:doc}, {new:true}, function(err,doc) {
-          if (err) {
-            console.log(err);
-            data.meta.code = 500;
-            data.error = "Internal Server Error"
-            res.send(500, data);
+        var profile = req.session.currentUserProfile;
+        delete profile.id;
+        console.log(2)
+        // data biasa
+        if (req.body["profile.phones"]) {
+          if (typeof(req.body["profile.phones"]) == "string") {
+            req.body["profile.phones"] = [ req.body["profile.phones"] ];
           }
-          if (doc) {
-            console.log(doc);
-            data.meta.code = 200;
-            data.error = "OK";
-            res.send(200, data);
+          profile.phones = req.body["profile.phones"];
+          // console.log("OLDPROFILE = " + JSON.stringify(oldProfile));
+        }
+        if (req.body['profile.npwp']) {
+          profile.npwp = req.body['profile.npwp'];
+        }
+        if (req.body['profile.nik']) {
+          profile.nik = req.body['profile.nik'];
+        }
+        if (req.body['profile.address']) {
+          profile.address = req.body['profile.address'];
+        }
+        if (req.body['profile.dates']) {
+          profile.dates = req.body['profile.dates'];
+        }
+        if (req.body['profile.website']) {
+          profile.website = req.body['profile.website'];
+        }
+        console.log(3)
+        // data profile
+        if (req.body["profile.socials.type"]) {
+          console.log(4)
+          var socials = [];
+          if (typeof(req.body["profile.socials.type"]) === "string") {
+            if (req.body["profile.socials.value"]) {
+              socials.push({
+                type: req.body["profile.socials.type"],
+                value: req.body["profile.socials.value"],
+              })
+            }
+          } else {
+            for (var i = 0; i < req.body["profile.socials.type"].length; i ++) {
+              if (req.body["profile.socials.value"][i]) {
+                socials.push({
+                  type: req.body["profile.socials.type"][i],
+                  value: req.body["profile.socials.value"][i],
+                })
+              }
+            }
           }
-        });
+        }
+        console.log(5)
+        // data avatar
+        if (req.files["profile.avatar"] && typeof(req.files["profile.avatar"]) !== "undefined" && req.files["profile.avatar"].name != "") {
+          console.log(6)
+          var exec = require('child_process').exec;
+          var child;
+          child = exec("mogrify -resize 512 " + req.files["profile.avatar"].path, function() {
+            var fileId = new ObjectID();
+            var store = app.store(fileId, "w");
+            var fd = fs.openSync(req.files["profile.avatar"].path, "r");
+            store.open(function(error, gridStore) {
+              gridStore.writeFile(fd, function(error, result) {
+                fs.unlinkSync(req.files["profile.avatar"].path);
+                profile.avatar = result.fileId;
+                console.log(7)
+                // data email
+                if (req.body["profile.emails"]) {
+                  if (typeof(req.body["profile.emails"]) == "string") {
+                    req.body["profile.emails"] = [ req.body["profile.emails"] ];
+                  }
+                  user.emailList(req.session.currentUser, function(list) {
+                    var deleted = [];
+                    var hash = {};
+                    for (var i = 0; i < req.body["profile.emails"].length; i++) {
+                      hash[req.body["profile.emails"][i]] = 1;
+                    }
+                    for (var i = 0; i < list.length; i ++) {
+                      if (hash[list[i].email] != 1) {
+                        deleted.push(list[i].email);
+                      }
+                    }
+                    associateEmails(0, req.session.currentUser, req, res, function() {
+                      console.log(8)
+                      if (deleted.length > 0) {
+                        user.disassociateEmail(req.session.currentUser, deleted, function(r) {
+                          console.log(profile);
+                          //console.log("REQBODY = " + JSON.stringify(req.body));
+                          
+                          var collection = settings.db.collection("user");
+                           collection.findAndModify({username: req.user.id}, {_id:1}, {$set:{'profile' : profile}}, {new:true}, function(err,doc) {
+                            if (err) {
+                              console.log(err);
+                              data.meta.code = 500;
+                              data.message = "Internal Server Error"
+                              res.send(500, data);
+                            }
+                            if (doc) {
+                              console.log(doc);
+                              data.meta.code = 200;
+                              data.message = "OK";
+                              res.send(200, data);
+                            }
+                          });
+                        })
+                      }
+                    })
+                  })
+                }else {
+                  if (error) {
+                    console.log(error);
+                    data.meta.code = 500;
+                    data.message = "Internal Server Error"
+                    res.send(500, data);
+                  }else {
+                    data.meta.code = 200;
+                    data.message = "OK";
+                    res.send(200, data);
+                  }
+                }
+              })
+            });
+          })
+        } else {
+          console.log(9)
+          // data email tanpa avatar
+          if (req.body["profile.emails"]) {
+            if (typeof(req.body["profile.emails"]) == "string") {
+              req.body["profile.emails"] = [ req.body["profile.emails"] ];
+            }
+            user.emailList(req.session.currentUser, function(list) {
+              console.log(10)
+              var deleted = [];
+              var hash = {};
+              for (var i = 0; i < req.body["profile.emails"].length; i++) {
+                hash[req.body["profile.emails"][i]] = 1;
+              }
+              for (var i = 0; i < list.length; i ++) {
+                if (hash[list[i].email] != 1) {
+                  deleted.push(list[i].email);
+                }
+              }
+              associateEmails(0, req.session.currentUser, req, res, function() {
+                console.log(11)
+                console.log('deleted',deleted)
+                if (deleted.length > 0) {
+                  user.disassociateEmail(req.session.currentUser, deleted, function(r) {
+                    console.log(profile);
+                    //console.log("REQBODY = " + JSON.stringify(req.body));
+                    
+                    var collection = settings.db.collection("user");
+                    collection.findAndModify({username: req.user.id}, {_id:1}, {$set:{'profile' : profile}}, {new:true}, function(err,doc) {
+                      console.log(12)
+                      if (err) {
+                        console.log(err);
+                        data.meta.code = 500;
+                        data.message = "Internal Server Error"
+                        res.send(500, data);
+                      }
+                      if (doc) {
+                        console.log(doc);
+                        data.meta.code = 200;
+                        data.message = "OK";
+                        res.send(200, data);
+                      }
+                    });
+                  })
+                }else {
+                  console.log(profile);
+                  //console.log("REQBODY = " + JSON.stringify(req.body));
+                  
+                  var collection = settings.db.collection("user");
+                  collection.findAndModify({username: req.user.id}, {_id:1}, {$set:{'profile' : profile}}, {new:true}, function(err,doc) {
+                    console.log(12)
+                    if (err) {
+                      console.log(err);
+                      data.meta.code = 500;
+                      data.message = "Internal Server Error"
+                      res.send(500, data);
+                    }
+                    if (doc) {
+                      console.log(doc);
+                      data.meta.code = 200;
+                      data.message = "OK";
+                      res.send(200, data);
+                    }
+                  });
+                }
+              })
+            })
+          }else {
+            console.log(profile);
+            //console.log("REQBODY = " + JSON.stringify(req.body));
+            
+            var collection = settings.db.collection("user");
+             collection.findAndModify({username: req.user.id}, {_id:1}, {$set:{'profile' : profile}}, {new:true}, function(err,doc) {
+              console.log(13)
+              if (err) {
+                console.log(err);
+                data.meta.code = 500;
+                data.message = "Internal Server Error"
+                res.send(500, data);
+              }
+              if (doc) {
+                console.log(doc);
+                data.meta.code = 200;
+                data.message = "OK";
+                res.send(200, data);
+              }
+            });
+          }
+        }
       }
     }
   }
